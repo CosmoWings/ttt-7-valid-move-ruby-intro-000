588533ba6df5410afdf37d9e71a53777ceb820fc
Patrick Pierre <patrick.pierre000@gmail.com>
2020-06-06 02:06:27 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,3 +2,14 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(array,index)
+  if array[index] == " "
+    return false
+  elsif array[index] == nil
+    return false
+  elsif array[index] == ""
+    return false
+  else
+    return true
+  end
+end
